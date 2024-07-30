bad4197298a5b5488d991ac94f5e6fb934c20356
Abdulrahman <abdulrahmanhe64@gmail.com>
2024-07-30 07:41:33 +0300

    2 comments

--- app/theme.tsx
+++ app/theme.tsx
@@ -5,12 +5,12 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: { 
-      main: '#23A6F0',
+      main: '#23A6F0',      // Blue
       light: '#E8F1FF',
       dark: '#A1C0F1',
     },
     secondary: {
-        main: '#252B42'
+        main: '#252B42'   // Dark blue: text + rating bg
     },
     text: {
         primary: '#252B42',
